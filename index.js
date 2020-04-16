b1b0c107f98cf6ccce584120be965ca12f77b139
Juanan26 <45574257+Juanan26@users.noreply.github.com>
2020-04-16 10:15:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const importsAPI = require(path.join(__dirname,"importsAPI"));
+const foodsImports = require(path.join(__dirname,"foodsImports"));
 const port = process.env.PORT || 80;
 
 console.log("Running module...");
@@ -12,6 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 importsAPI(app);
+foodsImports(app);
 
 app.listen(port, () => {
 	console.log("Server ready");
