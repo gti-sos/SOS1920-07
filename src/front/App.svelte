e9835358cadc15311237dd2a2306f13b76f6ff1c
Juanan26 <45574257+Juanan26@users.noreply.github.com>
2020-05-07 14:51:35 +0200

    Update App.svelte

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -29,3 +29,24 @@
 	<h1>Apis del Grupo 7 SOS</h1>
 	<Router {routes} />
 </main>
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1.a {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
