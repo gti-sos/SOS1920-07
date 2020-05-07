eac75ed73bea861f22ca637e06b72e48c24a7f06
Juanan26 <45574257+Juanan26@users.noreply.github.com>
2020-05-07 14:47:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const importsAPI = require(path.join(__dirname,"./src/back/importsAPI"));
+const foodsImports = require(path.join(__dirname,"./src/back/foodsImports"));
 var app = express();
 const fertilizerImportsExportsAPI = require(path.join(__dirname,"fertilizerImportsExports"));
 const port = process.env.PORT || 80;
@@ -10,6 +11,7 @@ const port = process.env.PORT || 80;
 app.use(bodyParser.json());
 
 importsAPI(app);
+foodsImports(app);
 fertilizerImportsExportsAPI(app);
 
 app.use("/", express.static("./public"));
