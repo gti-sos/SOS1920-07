ed3a32032ecde884c11d1c0fb8a1c03a6e2013a2
Juanan26 <45574257+Juanan26@users.noreply.github.com>
2020-05-07 14:11:08 +0200

    Update App.svelte

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -5,7 +5,11 @@
 	import EditImport from './ImportsGUI/EditImport.svelte';
 
 	import NotFound from './NotFound.svelte';
-
+	
+	import FoodImportsTable from './foodsImportsGUI/FoodImportsTable.svelte';
+	import EditFoodsImports from './foodsImportsGUI/EditFoodsImports.svelte';
+	import SearchFoodsImport from './foodsImportsGUI/SearchFoodsImport.svelte';
+	
 	import Home from './Home.svelte';
 	import GUIIMPORTS from './ImportsGUI/Home.svelte';
 
@@ -13,6 +17,9 @@
 		"/": Home,
 		"/import/:country/:year": EditImport,
 		"/import":ImportsTable,
+		"/foodsImports": FoodImportsTable,
+		"/foodsImports/:foodsName/:foodsYear":EditFoodsImports,
+		"/foodsImports/SearchFoodsImport/":SearchFoodsImport,
 		"/importsgui": GUIIMPORTS,
 		"*": NotFound
 	};
