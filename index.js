0733e9344a7ce5ea990ab6f7f1825a296d3bbd3a
jorvilaco <44461404+jorvilaco@users.noreply.github.com>
2020-05-07 12:44:30 +0200

    subit

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const path = require("path");
 
 const importsAPI = require(path.join(__dirname,"./src/back/importsAPI"));
 var app = express();
-const foodsImports = require(path.join(__dirname,"foodsImports"));
 const fertilizerImportsExportsAPI = require(path.join(__dirname,"fertilizerImportsExports"));
 const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 
 importsAPI(app);
-foodsImports(app);
 fertilizerImportsExportsAPI(app);
 
 app.use("/", express.static("./public"));
