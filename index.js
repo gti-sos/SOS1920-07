caded02ad1ed4bec6c7fee2e63137f554bb4c485
jorvilaco <44461404+jorvilaco@users.noreply.github.com>
2020-05-07 20:42:54 +0200

    subir

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const importsAPI = require(path.join(__dirname,"./src/back/importsAPI"));
 const foodsImports = require(path.join(__dirname,"./src/back/foodsImports"));
 var app = express();
-const fertilizerImportsExportsAPI = require(path.join(__dirname,"./src/back/fertilizerImportsExports"));
+const fertilizerImportsExportsAPI = require(path.join(__dirname,"./src/back/fertilizerImportsExportsAPI"));
 const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
