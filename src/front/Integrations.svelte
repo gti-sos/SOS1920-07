1304ad7a218148b249316bc2a53fc9db1bee238d
pablopaz1997 <56607017+pablopaz1997@users.noreply.github.com>
2020-06-01 10:48:36 +0200

    Update Integrations.svelte

--- src/front/Integrations.svelte
+++ src/front/Integrations.svelte
@@ -12,3 +12,9 @@
     <Button outline size="lg" color="secondary" onclick="location.href='/#/imports-integrations';" >Integración</Button>
     <Button outline size="lg" color="secondary" onclick="location.href='/#/import-table';" >Tabla Api</Button>
 </div>
+<div style='margin-top:50px;'>
+    <h2>Integraciones de Pablo</h2>
+    <Button outline size="lg" color="danger" onclick="location.href='/#/fertilizerImportsExports/MyGraph';" >Gráfica Highchart</Button>
+    <Button outline size="lg" color="danger" onclick="location.href='/#/fertilizerImportsExports/MyGraph2';" >Gráfica 2</Button>
+    <Button outline size="lg" color="danger" onclick="location.href='/#/fertilizerImportsExports/Integrations';" >Integraciones</Button>
+</div>
